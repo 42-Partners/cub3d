251784f71a2e6d2896b050b160b3a6d6e8f6b936
Gus1331 <gustavooliver.souza@gmail.com>
2026-03-10 20:36:53 -0300

    fix: project name on CI + norminette

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -11,9 +11,9 @@
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <stdio.h>
-#include "MLX42/MLX42.h"
+# include <stdio.h>
+# include "MLX42/MLX42.h"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -12,7 +12,8 @@
 
 #include "cub3d.h"
 
-int	main(int argc, char *argv[]) {
+int	main(int argc, char *argv[])
+{
 	(void)argc;
 	(void)argv;
 	printf("Hello World!\n");
