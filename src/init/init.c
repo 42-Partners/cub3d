f767edc7f5f551c516ee27ea6ae0613e95a29499
Rafaelly <rafaellypoliveira@gmail.com>
2026-03-11 18:23:40 -0300

    feat: add MLX42 main loop

--- src/init/init.c
+++ src/init/init.c
@@ -6,7 +6,7 @@
 /*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 16:06:10 by devrafaelly       #+#    #+#             */
-/*   Updated: 2026/03/11 18:11:47 by devrafaelly      ###   ########.fr       */
+/*   Updated: 2026/03/11 18:21:09 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,6 @@ void	init_game(t_game *game)
 	if (!game->mlx)
 		error_exit(game, NULL);
 	mlx_close_hook(game->mlx, close_window, game);
+	mlx_key_hook(game->mlx, handle_key, game);
+	mlx_loop(game->mlx);
 }
