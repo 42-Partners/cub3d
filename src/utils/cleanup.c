bec5faf601f8384684925ef295bc3888933d26b7
Rafaelly <rafaellypoliveira@gmail.com>
2026-03-11 18:07:11 -0300

    feat: add cleanup and error_exit utility functions

---
+++ src/utils/cleanup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/11 16:18:39 by devrafaelly       #+#    #+#             */
+/*   Updated: 2026/03/11 17:56:15 by devrafaelly      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+#include <stdlib.h>
+
+void	cleanup(t_game *game)
+{
+	mlx_terminate(game->mlx);
+	return ;
+}
+
+void	error_exit(t_game *game, char *msg)
+{
+	if (msg)
+		printf("Error\n%s\n", msg);
+	else
+		printf("Error\n%s\n", mlx_strerror(mlx_get_errno()));
+	cleanup(game);
+	exit(EXIT_FAILURE);
+}
+// void mlx_close_window(mlx_t* mlx)
