b3271e0192a5252557b2e22b64f4624f5bec1bf6
Rafaelly <rafaellypoliveira@gmail.com>
2026-03-11 18:16:10 -0300

    feat: add t_game struct and MLX42 window initialization

--- src/init/init.c
+++ src/init/init.c
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cleanup.c                                          :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/11 16:18:39 by devrafaelly       #+#    #+#             */
-/*   Updated: 2026/03/11 18:07:08 by devrafaelly      ###   ########.fr       */
+/*   Created: 2026/03/11 16:06:10 by devrafaelly       #+#    #+#             */
+/*   Updated: 2026/03/11 18:11:47 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-#include <stdlib.h>
-
-void	cleanup(t_game *game)
+void	init_game(t_game *game)
 {
-	mlx_terminate(game->mlx);
-	return ;
-}
-
-void	error_exit(t_game *game, char *msg)
-{
-	if (msg)
-		printf("Error\n%s\n", msg);
-	else
-		printf("Error\n%s\n", mlx_strerror(mlx_get_errno()));
-	cleanup(game);
-	exit(EXIT_FAILURE);
+	game->mlx = mlx_init(WIDTH, HEIGHT, "cub3D", RESIZE);
+	if (!game->mlx)
+		error_exit(game, NULL);
+	mlx_close_hook(game->mlx, close_window, game);
 }
