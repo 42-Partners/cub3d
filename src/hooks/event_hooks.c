423380c2ea8420647353a4cbb8da31d3bd7380da
Rafaelly <rafaellypoliveira@gmail.com>
2026-03-11 18:24:38 -0300

    feat: add ESC key hook to close window

--- src/hooks/event_hooks.c
+++ src/hooks/event_hooks.c
@@ -6,7 +6,7 @@
 /*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 16:44:09 by devrafaelly       #+#    #+#             */
-/*   Updated: 2026/03/11 18:09:47 by devrafaelly      ###   ########.fr       */
+/*   Updated: 2026/03/11 18:21:14 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,10 @@ void	close_window(void *param)
 	game = (t_game *)param;
 	cleanup(game);
 	exit(EXIT_SUCCESS);
+}
+
+void	handle_key(mlx_key_data_t keydata, void *param)
+{
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		close_window(param);
 }
