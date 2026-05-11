67709b57412bbd8c0e4f5250cd3e29ff483f19f7
Gus1331 <gustavooliver.souza@gmail.com>
2026-05-11 01:46:44 -0300

    debug: cd

--- src/main.c
+++ src/main.c
@@ -18,9 +18,13 @@ int	main(int argc, char *argv[])
 
 	ft_memset(&game, 0, sizeof(t_game));
 	validate_file(&game, argc, argv[1]);
+	ft_printf("[DEBUG]: FILE VALIDATED\n");
 	validate_map(&game);
+	ft_printf("[DEBUG]: MAP VALIDATED\n");
 	init_player_vectors(&game);
+	ft_printf("[DEBUG]: PLAYER VECTOR INITIATED\n");
 	init_game(&game);
+	ft_printf("[DEBUG]: GAME ON\n");
 	mlx_loop(game.mlx);
 	return (0);
 }
