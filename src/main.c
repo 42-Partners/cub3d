598fc195c4bab4dc6ba30dc78dce03f2e65cfbc3
Gus1331 <gustavooliver.souza@gmail.com>
2026-05-11 03:01:19 -0300

    fix: infinite loading screen emscript cd

--- src/main.c
+++ src/main.c
@@ -12,15 +12,6 @@
 
 #include "cub3d.h"
 
-void	debug_heartbeat(void *param)
-{
-	static int	frame = 0;
-
-	frame++;
-	if (frame % 60 == 0)
-		printf("[DEBGU] MLX_LOOP!  frame: %d\n", frame);
-}
-
 int	main(int argc, char *argv[])
 {
 	t_game	game;
@@ -30,8 +21,6 @@ int	main(int argc, char *argv[])
 	validate_map(&game);
 	init_player_vectors(&game);
 	init_game(&game);
-	printf("[WEB] Tudo inicializado. Entrando no mlx_loop...\n");
-	mlx_loop_hook(game.mlx, debug_heartbeat, &game);
 	mlx_loop(game.mlx);
 	return (0);
 }
